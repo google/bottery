8d7c694b0e142913221e02a170192cac61f1220d
Shubham Mishra <shubhammshr621@gmail.com>
2017-10-31 18:16:03 -0700

    Setting up dev environment to run bottery on local machine watching any change in files and live-reloading

---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = function(grunt){
+  var protocol = grunt.option('https')?'https' : 'http';
+  var liveReloadPort = grunt.option('liveReloadPort') || 35730;
+  var serverPort = grunt.option('port') || 9000;
+  var HOST_IP = 'localhost';
+  grunt.initConfig({
+    connect: {
+      options: {
+        port: serverPort,
+        hostname: HOST_IP,
+        base: './src',
+        protocol : protocol
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              require('connect-livereload')({ port: liveReloadPort }),
+              require('serve-static')('./')
+            ];
+          }
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: liveReloadPort
+      },
+      files: [
+        '/js/**/*.js',
+        '/bots/**/*.js',
+        '/css/**/*.css',
+        '!/js/vendor/*',
+      ]
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('serve', [
+    'connect:livereload',
+    'watch'
+  ]);
+}
