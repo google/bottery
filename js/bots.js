2ce0c4aeb55802f37557ed510a4b2d0bea4093ec
Noah Leigh <noah.d.leigh@gmail.com>
2017-11-02 11:26:15 -0400

    Bot load logs use informative logging methods, string formatting

--- js/bots.js
+++ js/bots.js
@@ -15,17 +15,17 @@
 // Load bots for demo
 
 function loadBot(name, map) {
-  console.log("loading " + name);
+  console.info("Loading bot '%s'", name);
   req = $.ajax("bots/" + name + ".js", {
     async: false,
     dataType: "script",
   });  
   req.done(function(data) {
-    console.log("Bot '" + name + "' loaded successfully");
+    console.info("Bot '%s' loaded successfully", name);
     map[name] = eval(data);
   });
-  req.fail(function( jqXHR, textStatus ) {
-    console.log("Bot '" + name + "' could not be loaded: " + textStatus );
+  req.fail(function(jqXHR) {
+    console.error("Bot '%s' could not be loaded: %d (%s)", name, jqXHR.status, jqXHR.statusText);
   });
 }
 
